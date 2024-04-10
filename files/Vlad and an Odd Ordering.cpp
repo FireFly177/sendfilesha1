f8a44cd35d809013b141d528b139bbdc95b42c7f
Duy Nguyen <huonghaiduynhim@gmail.com>
2024-04-10 22:37:28 +0700

    first commit

---
+++ files/Vlad and an Odd Ordering.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int tc; cin >> tc;
+
+    while(tc--) {
+        int n, k;
+        cin >> n >> k;
+
+    }
+
+    return 0;
+}
